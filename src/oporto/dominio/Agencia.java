00361bb85bf133eb3f3585191ef14b908b44dd95
rodrigooporto <rodrigooporto@gmail.com>
2020-10-15 08:47:44 -0300

    Criação das classes Banco e Agencia

---
+++ src/oporto/dominio/Agencia.java
@@ -0,0 +1,13 @@
+package oporto.dominio;
+
+public class Agencia extends Banco{
+	private String numeroAgencia;
+
+	public String getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
+	public void setNumeroAgencia(String numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+}
--- src/oporto/dominio/Cliente.java
+++ src/oporto/dominio/Cliente.java
@@ -5,4 +5,17 @@ public class Cliente {
 	private String nome;
 	private Date dataNascimento;
 	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}	
 }
