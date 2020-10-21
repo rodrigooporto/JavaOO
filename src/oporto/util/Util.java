ee4e1d41978ea7dce3859099b15f3acb3e70ae54
Rodrigo Oliveira Porto <rodrigooporto@gmail.com>
2020-10-21 10:01:16 -0300

    Update Util.java
    Comentário para testar egit

--- src/oporto/util/Util.java
+++ src/oporto/util/Util.java
@@ -3,6 +3,7 @@ package oporto.util;
 import java.util.Scanner;
 
 public class Util {
+	//Comentario para testar o comportamento do Egit
 	public static Scanner entrada = new Scanner(System.in);
 	
 	public static void saidaTexto(String mensagem) {
